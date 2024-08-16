415f7bc524608d4acc3c4df8537a1af4ec365391
Felipe Costa <felipe.costa@mercadolibre.com>
2024-08-16 19:02:44 -0300

    Added precommit

---
+++ src/test/java/com/uade/propertiesbackend/Testing.java
@@ -0,0 +1,12 @@
+package com.uade.propertiesbackend;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Testing {
+  @Test
+  void test(){
+    assertEquals(1,2);
+  }
+}
