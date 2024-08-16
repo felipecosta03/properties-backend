c6e5b0220208f13f5217d4bdb52c2325c5f7d010
Felipe Costa <felipe.costa@mercadolibre.com>
2024-08-16 17:51:31 -0300

    Initialize scaffolding

---
+++ src/main/java/com/uade/propertiesbackend/PropertiesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uade.propertiesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PropertiesBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/uade/propertiesbackend/PropertiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.propertiesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertiesBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
